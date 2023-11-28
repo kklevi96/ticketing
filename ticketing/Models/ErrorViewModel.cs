0ad1e290256286521cb482b913c439fabebea836
hallgato <hallgato@LABOR215-02>
2023-11-28 15:45:52 +0100

    Add project files.

---
+++ ticketing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ticketing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
