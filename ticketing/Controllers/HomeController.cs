b5a8a29f0f4d0becf0b007f999e21bf2a47c5281
kklevi <kklevi@gmail.com>
2023-11-28 16:56:17 +0100

    update

--- ticketing/Controllers/HomeController.cs
+++ ticketing/Controllers/HomeController.cs
@@ -7,15 +7,47 @@ namespace ticketing.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public List<string> _gondok;
 
         public IActionResult Index()
         {
-            return View();
+            List<string> gondok = ReadGondokFromFile();
+            return View(gondok);
+        }
+
+        private readonly string _gondokFilePath = "wwwroot/gondok.txt";
+
+        [HttpPost]
+        public IActionResult GondSubmitted(string gond)
+        {
+            // Itt kezeld a beküldött "gond"-ot (mentés, stb.)
+            // Például mentés egy adatbázisban vagy listában
+
+            // Frissítsd a gondok listát
+            List<string> gondok = ReadGondokFromFile();
+            gondok.Add(gond);
+
+            // Írd vissza a frissített gondokat a fájlba
+            WriteGondokToFile(gondok);
+
+            return RedirectToAction("Index");
+        }
+
+        private List<string> ReadGondokFromFile()
+        {
+            if (System.IO.File.Exists(_gondokFilePath))
+            {
+                return System.IO.File.ReadAllLines(_gondokFilePath).ToList();
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+
+        private void WriteGondokToFile(List<string> gondok)
+        {
+            System.IO.File.WriteAllLines(_gondokFilePath, gondok);
         }
 
         public IActionResult Privacy()
